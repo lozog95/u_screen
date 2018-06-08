7fac5521c06de7127ef729727b43e8872e5c4cd5
lozog95 <lozog95@gmail.com>
2018-06-09 01:02:03 +0200

    first

---
+++ u_screen.py
@@ -0,0 +1,14 @@
+import pyscreenshot as ps
+from pynput.mouse import Listener
+from datetime import datetime
+
+def on_click(x,y, button, pressed):
+	now=datetime.now()
+	h=str(now.hour)
+	m=str(now.minute)
+	s=str(now.second)
+	ss=ps.grab()
+	ss.save('/Users/lozog/screen'+h+"_"+m+"_"+s+'.png')
+
+with Listener (on_click=on_click) as listener:
+	listener.join()
